37b6cbdf6fe7eec22432d5697eddc13bf5703f3a
Marko P <markop@mailbox.org>
2021-10-06 00:09:52 +0200

    Created basic project_setup unit tests

--- suites/unit/test_project_setup.py
+++ suites/unit/test_project_setup.py
@@ -5,29 +5,41 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as ec
 from selenium.webdriver.support.wait import WebDriverWait as WdWait
 
+from helpers import utils
 from suites.base_test import BaseTest
+from css_selectors import ProjectSetup as proj_css
 
 
-class TestLoginPage(BaseTest):
+class TestProjectSetupUnit(BaseTest):
     """
     Basic authentication suite
     """
-    main_page_cond = ec.visibility_of_element_located((By.CSS_SELECTOR, 'div#main_header'))
 
     def setUp(self) -> None:
-        super(TestLoginPage, self).setUp()
-        load_dotenv('../../.env')
-        email = os.environ.get('tribe_regular_email')
-        password = os.environ.get('tribe_regular_password')
-        self.driver.find_element(by=By.ID, value='email').send_keys(email)
-        self.driver.find_element(by=By.ID, value='password').send_keys(password)
-        self.driver.find_element(by=By.CSS_SELECTOR, value='button.Button').click()
+        super(TestProjectSetupUnit, self).setUp()
+        super(TestProjectSetupUnit, self).login()
 
     def tearDown(self) -> None:
-        super(TestLoginPage, self).tearDown()
+        super(TestProjectSetupUnit, self).tearDown()
 
     def test_page_load(self):
-        WdWait(self.driver, 10).until(self.main_page_cond)
+        proj_row_first = ec.visibility_of_element_located((By.CSS_SELECTOR, proj_css.job_row_nth(1)))
+        WdWait(self.driver, 10).until(proj_row_first)
 
-    def test_add_job_popup(self):
-        pass
+    def test_job_add_popup(self):
+        job_popup = ec.visibility_of_element_located((By.CSS_SELECTOR, proj_css.job_add_popup))
+        self.driver.find_element(by=By.CSS_SELECTOR, value=proj_css.job_add_button).click()
+        WdWait(self.driver, 10).until(job_popup)
+
+    def test_company_add_popup(self):
+        company_popup = ec.visibility_of_element_located((By.CSS_SELECTOR, proj_css.company_add_popup))
+        self.driver.find_element(by=By.CSS_SELECTOR, value=proj_css.company_add_button).click()
+        WdWait(self.driver, 10).until(company_popup)
+
+    def test_job_edit_popup(self):
+        edit_job_button_ec = ec.visibility_of_element_located((By.CSS_SELECTOR, proj_css.edit_job_nth(1)))
+        edit_popup_ec = ec.visibility_of_element_located((By.CSS_SELECTOR, proj_css.job_edit_popup))
+
+        edit_job_button = WdWait(self.driver, 10).until(edit_job_button_ec)
+        edit_job_button.click()
+        WdWait(self.driver, 10).until(edit_popup_ec)
