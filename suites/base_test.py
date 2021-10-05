86a0f15776b39b8f18b5dbeffb706a7db907dd9a
Marko P <markop@mailbox.org>
2021-10-06 00:08:23 +0200

    Added login method to base_test.py

--- suites/base_test.py
+++ suites/base_test.py
@@ -1,5 +1,7 @@
+import os
 from unittest import TestCase
 
+from dotenv import load_dotenv
 from selenium.common.exceptions import UnexpectedAlertPresentException
 from selenium.webdriver import Chrome
 from selenium.webdriver.chrome.options import Options
@@ -8,18 +10,24 @@ from selenium.webdriver.support import expected_conditions as ec
 from selenium.webdriver.support.wait import WebDriverWait as WdWait
 from webdriver_manager.chrome import ChromeDriverManager
 
+from helpers import utils
+from css_selectors import Auth as auth_css
+from css_selectors import General as gen_css
+
 
 class BaseTest(TestCase):
 
     def setUp(self) -> None:
         options = Options()
         options.headless = True
-        self.driver = Chrome(executable_path=ChromeDriverManager(log_level=0, print_first_line=False).install(),
+        os.environ['WDM_LOG_LEVEL'] = '0'
+        os.environ['WDM_PRINT_FIRST_LINE'] = 'False'
+        self.driver = Chrome(executable_path=ChromeDriverManager().install(),
                              options=options)
         self.driver.set_window_size(1920, 1080)
         self.driver.maximize_window()
         self.driver.get('https://overview.tribe.xyz/')
-        WdWait(self.driver, 10).until(ec.visibility_of_element_located((By.CSS_SELECTOR, 'div.main-page')))
+        WdWait(self.driver, 10).until(ec.visibility_of_element_located((By.CSS_SELECTOR, gen_css.main_page)))
 
     def tearDown(self) -> None:
         # TODO - customize the name of the test_screenshot so it is instead the name of the test that called it + time
@@ -30,3 +38,17 @@ class BaseTest(TestCase):
             self.driver.save_screenshot('test_screenshot2.png')
 
         self.driver.quit()
+
+    def login(self):
+        main_page_cond = ec.visibility_of_element_located((By.CSS_SELECTOR, gen_css.main_header))
+
+        dot_env = utils.get_dot_env()
+        load_dotenv(dot_env)
+        email = os.environ.get('tribe_regular_email')
+        password = os.environ.get('tribe_regular_password')
+
+        self.driver.find_element(by=By.CSS_SELECTOR, value=auth_css.email_input).send_keys(email)
+        self.driver.find_element(by=By.CSS_SELECTOR, value=auth_css.password_input).send_keys(password)
+        self.driver.find_element(by=By.CSS_SELECTOR, value=auth_css.login_button).click()
+        WdWait(self.driver, 10).until(main_page_cond)
+
